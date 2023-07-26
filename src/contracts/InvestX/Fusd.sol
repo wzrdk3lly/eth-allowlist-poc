406b9fbc742fba8b2153109a73f65dfcb78fad1b
wzrdk3lly <dkmk1149@gmail.com>
2023-07-26 14:31:07 -0400

    ADD InvestX logic

--- src/contracts/InvestX/Fusd.sol
+++ src/contracts/InvestX/Fusd.sol
@@ -1,2 +1,10 @@
 // SPDX-License-Identifier: Unlicense
+import {ERC20} from "openzeppelin-contracts/token/ERC20/ERC20.sol";
+
 pragma solidity ^0.8.11;
+
+contract Fusd is ERC20 {
+    constructor() ERC20("FakeUSD", "FUSD") {
+        _mint(msg.sender, 2000);
+    }
+}
--- src/contracts/InvestX/InvestX.sol
+++ src/contracts/InvestX/InvestX.sol
@@ -1,5 +1,22 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.11;
 
-contract InvestX {}
+import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
+
+contract InvestX {
+    IERC20 public fusd;
+    address public fusdPool;
+
+    constructor(address _fusd, address _fusdPool) {
+        fusd = IERC20(_fusd);
+        fusdPool = _fusdPool;
+    }
+
+    /**
+     *
+     * @param _amountIn amount of FUSD that a user transfers into the protocol
+     */
+    function investFusd(uint256 _amountIn) public {
+        fusd.transferFrom(msg.sender, fusdPool, _amountIn);
+    }
+}
